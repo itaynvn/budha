6325b50cfd03979825a2ce753a0fe7398a34dc3f
itaynvn <61652839+itaynvn@users.noreply.github.com>
2020-03-24 20:13:10 +0200

    Update send_invites.rb

--- send_invites.rb
+++ send_invites.rb
@@ -1,13 +1,5 @@
 # select organization into var
-o=Membership.where(slug:"Winter2020")
 
 # send email invitation for organization
-Membership.create_membership(o, "email@address.com", 2)
 
 # delete memberships (revoke access) to all users in organization
-Membership.where(organization_id: 9).each do |ma|
-       to_remove << ma
-end
-to_remove.each do |t|
-    t.delete
-end
